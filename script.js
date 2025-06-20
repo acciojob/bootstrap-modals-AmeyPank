2418cb6d30adca8cd77ab937f450f709aec98262
Amey Pankanti <pankantiameya@gmail.com>
2025-06-20 14:02:30 +0530

    Ans

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+// <!-- Trigger Modal via JavaScript -->
+const openBtn = document.getElementById('open-modal');
+const newtonModal = new bootstrap.Modal(document.getElementById('newton-modal'));
+
+openBtn.addEventListener('click', () => {
+    newtonModal.show();
+});
