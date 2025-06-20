28866e6c4759496969246553d4448d05a711e2f6
Amey Pankanti <pankantiameya@gmail.com>
2025-06-20 14:04:31 +0530

    Update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your code here
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script> */
 // <!-- Trigger Modal via JavaScript -->
 const openBtn = document.getElementById('open-modal');
 const newtonModal = new bootstrap.Modal(document.getElementById('newton-modal'));
